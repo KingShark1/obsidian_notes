c5da511db5298234f157a13fbae705821c1eaba1
KingShark1 <manastiwari28@gmail.com>
2023-08-09 09:09:57 +0530

    Commit on Wednesday 09 August 2023 09:09:57 AM IST

--- upload_script.sh
+++ upload_script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+current_date_time=$(date)
 git add .
-git commit -m "Commit on $date"
+git commit -m "Commit on $current_date_time"
 git push git@github.com:KingShark1/obsidian_notes.git HEAD:main
